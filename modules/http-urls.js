d65c669f8b1c508c46f0a6898f28e8bb2fdfb72c
Augustin Cavalier <waddlesplash@gmail.com>
2014-07-24 15:00:06 -0400

    HTTP-urls: use a better regex and escape stuff.

--- modules/http-urls.js
+++ modules/http-urls.js
@@ -8,10 +8,57 @@
 var http = require("http");
 var https = require("https");
 
+/* From PHPJS */
+function htmlspecialchars_decode(string, quote_style) {
+  var optTemp = 0,
+  i = 0,
+  noquotes = false;
+  if (typeof quote_style === 'undefined') {
+    quote_style = 2;
+  }
+  string = string.toString()
+  .replace(/&lt;/g, '<')
+  .replace(/&gt;/g, '>');
+  var OPTS = {
+    'ENT_NOQUOTES': 0,
+    'ENT_HTML_QUOTE_SINGLE': 1,
+    'ENT_HTML_QUOTE_DOUBLE': 2,
+    'ENT_COMPAT': 2,
+    'ENT_QUOTES': 3,
+    'ENT_IGNORE': 4
+  };
+  if (quote_style === 0) {
+    noquotes = true;
+  }
+  if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
+    quote_style = [].concat(quote_style);
+    for (i = 0; i < quote_style.length; i++) {
+      // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
+      if (OPTS[quote_style[i]] === 0) {
+        noquotes = true;
+      } else if (OPTS[quote_style[i]]) {
+        optTemp = optTemp | OPTS[quote_style[i]];
+      }
+    }
+    quote_style = optTemp;
+  }
+  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+    string = string.replace(/&#0*39;/g, "'"); // PHP doesn't currently escape if more than one 0, but it should
+    // string = string.replace(/&apos;|&#x0*27;/g, "'"); // This would also be useful here, but not a part of PHP
+  }
+  if (!noquotes) {
+    string = string.replace(/&quot;/g, '"');
+  }
+  // Put this in last place to avoid escape being double-decoded
+  string = string.replace(/&amp;/g, '&');
+  
+  return string;
+}
+
 exports.onMessage = function(channelSettings, globalSettings, parameters) {
-  var taskPattern = /https?/g;
   var urls = [], match;
-  var regexp = /(\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  var regexp = /(https?:\/\/[^\s]+)/g;
+  console.log(parameters.message);
   while(matches = regexp.exec(parameters.message)) {
     if((matches[0].indexOf("127.0.0.1") == -1) && (matches[0].indexOf("localhost") == -1))
       urls.push(matches[0]);
@@ -34,7 +81,7 @@ exports.onMessage = function(channelSettings, globalSettings, parameters) {
         if(pos == -1) return;
 
         title = title.slice(0, pos);
-        parameters.bot.say(parameters.channel, "..." + url.slice(-10) + ': ' + title.trim());
+        parameters.bot.say(parameters.channel, url.slice(0, 10) + "..." + url.slice(-6) + ': ' + htmlspecialchars_decode(title.trim()));
       }
       response.on('data', function (chunk) {
         data += chunk;
