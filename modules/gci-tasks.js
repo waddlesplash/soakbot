7f4a6cf2d24ea76a1479775d915c58b237691363
Augustin Cavalier <waddlesplash@gmail.com>
2014-12-02 10:58:10 -0500

    GCI-tasks: only show one line of text.
    Also, no need to print the ID as it's in the link.

--- modules/gci-tasks.js
+++ modules/gci-tasks.js
@@ -19,12 +19,12 @@ exports.onMessage = function(channelSettings, globalSettings, parameters) {
     // Remove URLs from the message so other modules don't do stuff with them
     parameters.message.replace(new RegExp("http://" + tasks[taski].join("/"), 'g'), '');
     parameters.message.replace(new RegExp("https://" + tasks[taski].join("/"), 'g'), '');
-    
+
     var task = tasks[taski];
     var options = {
       host: task[0],
       path: "/"+(task.slice(1).join("/"))
-    }; 
+    };
     var callback = function(response) {
       var data = '';
 
@@ -40,20 +40,12 @@ exports.onMessage = function(channelSettings, globalSettings, parameters) {
         var status = $(".status .emph").text();
         var mentors = $(".mentor .emph").text().split(", ");
         var tags = $(".tags .emph").text().split(", ");
-        var first = {
-          num: parseInt($(".time-first .number").text(), 10),
-          unit: $(".time-first .cap").text()
-        };
-        var second = {
-          num: parseInt($(".time:not(.time-first) .number").text(), 10),
-          unit: $(".time:not(.time-first) .cap").text()
-        };
-        parameters.bot.say(parameters.channel, project+": "+title+" ("+status+")");
-        parameters.bot.say(parameters.channel, "Mentor"+(mentors.length > 1 ? "s" : "")+": "+mentors.join(", "));
-        if(!(isNaN(first.num) || isNaN(second.num)))
-          parameters.bot.say(parameters.channel, first.num+" "+first.unit+", "+second.num+" "+second.unit+" remaining");
+        var say = project + ": " + title.replace(/ \(................\)/, "");
+        say += " | " + status;
+        say += " | Mentor"+(mentors.length > 1 ? "s" : "") + ": " + mentors.join(", ");
+        parameters.bot.say(parameters.channel, say);
       });
     }
-    http.request(options, callback).end(); 
+    http.request(options, callback).end();
   }
 };
