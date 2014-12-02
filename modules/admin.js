c5b5b0fec376750a2e7c6c0bbd5d53e751813878
Augustin Cavalier <waddlesplash@gmail.com>
2014-12-02 11:16:43 -0500

    Admin: reduce limit to 5 lines.

--- modules/admin.js
+++ modules/admin.js
@@ -3,7 +3,7 @@
  *   DANGER: make sure the admins have NickServ kill protection on,
  *           or lock the bot inside a CHROOT, because this module allows
  *           execution of shell commands!!
- * 
+ *
  * Licensed under the MIT license.
  */
 var exec = require('child_process').exec;
@@ -11,13 +11,13 @@ var exec = require('child_process').exec;
 exports.onMessage = function(parameters) {
   function puts(error, stdout, stderr) {
     var lineCount = (stdout.match(/\n/g)||[]).length + (stderr.match(/\n/g)||[]).length;
-    if(lineCount < 15) {
+    if(lineCount < 5) {
       parameters.bot.say(parameters.channel, stderr);
       parameters.bot.say(parameters.channel, stdout);
     } else
       parameters.bot.say(parameters.channel, "Command output too long (" + lineCount + " lines)!");
   }
-  
+
   var trimmed = parameters.message.trim();
   if(trimmed.indexOf("$(") == 0) {
     var cmd = trimmed.substring(2, trimmed.length - 1);
